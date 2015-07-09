3a7ab2c46c1350f375250113a7122287b0b982b9
ivaldojr <ivaldo.junior@spread.com.br>
2015-07-09 15:23:32 -0300

    start project

---
+++ CompareClass.Net/Compare.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareClass.Net
+{
+   public class Compare
+    {
+
+        static bool isEqual { get; set; }
+
+        public static void Compare<T>(object class1, object class2)
+        {
+       
+            var Class1Properties = class1 != null ? class1.GetType().GetProperties() : null;
+            var Class1PropertiesValues = class1 != null ? Class1Properties.Select(n => n.GetValue(class1, null)) : null;
+
+            var Class2Properties = class2 != null ? class2.GetType().GetProperties() : null;
+            var Class2PropertiesValues = class2 != null ? Class2Properties.Select(n => n.GetValue(class2, null)) : null;
+
+
+            isEqual = Class1PropertiesValues.SequenceEqual(Class2PropertiesValues);
+
+
+            if (!isEqual)
+            {
+                for (int i = 0; i < Class1PropertiesValues.Count(); i++)
+                {
+
+                    if (Convert.ToString(GetPropValue(class1, Class1Properties[i].Name)) != Convert.ToString(GetPropValue(class2, Class2Properties[i].Name)))
+                    {
+                        
+                        Console.WriteLine(Convert.ToString(GetPropValue(class1, Class1Properties[i].Name)));
+                        Console.WriteLine(Convert.ToString(GetPropValue(class2, Class1Properties[i].Name)));
+                    }
+
+
+                }
+            }
+
+          
+        }
+
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+    
+   }
+
+
+
+}
---
+++ TestCompareClass.Net/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompareClass.Net;
+
+namespace TestCompareClass.Net
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            model m1 = new model() { age = 2, name = "Sandy" };
+            model m2 = new model() { age = 5, name = "Rauff" };
+
+            Compare.Compare<model>(m1, m2);
+
+
+        }
+    }
+
+
+    public class model
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+
+
+
+}
